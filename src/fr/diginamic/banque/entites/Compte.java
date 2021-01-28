09a85abf9e264789169444450758b3fcbb5b6539
nicolasH <nicolas.hornuel@gmail.com>
2021-01-28 12:27:26 +0100

    J2-TP6-@override

--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -9,9 +9,25 @@ public class Compte {
 			numCompte = n;
 			soldeCompte = s;	
 		}
-		
+
+		public int getNumCompte() {
+			return numCompte;
+		}
+
+		public void setNumCompte(int numCompte) {
+			this.numCompte = numCompte;
+		}
+
+		public double getSoldeCompte() {
+			return soldeCompte;
+		}
+
+		public void setSoldeCompte(double soldeCompte) {
+			this.soldeCompte = soldeCompte;
+		}
+
 		@Override
 		public String toString() {
-			return "N° du Compte : " + numCompte + ". Solde du Compte : " + soldeCompte;
+			return "N° du Compte : " + numCompte + ". Solde du Compte : " + soldeCompte + "€";
 		}
 }
