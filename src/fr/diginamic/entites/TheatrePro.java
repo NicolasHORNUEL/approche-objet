dcaa786794d8748ceca2edff7e73c0b4990873c5
nicolasH <nicolas.hornuel@gmail.com>
2021-02-01 15:00:14 +0100

    TP-J3&J4-THEATREPRO

---
+++ src/fr/diginamic/entites/TheatrePro.java
@@ -0,0 +1,80 @@
+package fr.diginamic.entites;
+
+public class TheatrePro {
+
+	private String nom;
+	private Spectacle[] tab;
+
+	public TheatrePro(String nom, Spectacle[] spectacles) {
+		this.nom = nom;
+		this.tab = spectacles;
+	}
+
+	public void ajouterSpectacle(Spectacle spectacle) {
+			Spectacle[] newTab = new Spectacle[tab.length+1];
+			newTab[tab.length] = spectacle;
+			for (int i = 0; i < tab.length; i++) {
+				newTab[i] = tab[i];			
+			}
+			tab = newTab;
+	}
+	
+	public void inscrire(String nomSpectacle, int nbClients) {
+		Spectacle spe = getSpectacle(nomSpectacle);
+		if ( spe != null) {
+			int nbInscrits = spe.getNbInscrits()+nbClients;
+			if (nbInscrits<=spe.getCapaciteMax()) {
+				spe.setNbInscrits(nbInscrits);
+			} else {
+				System.err.println("Complet!");
+			}
+		} else {
+			System.err.println("Spectacle inconnu!");
+		}
+	}
+	
+	public double getRecetteTotale() {
+		double recette = 0.0;
+		for (int i = 0; i < tab.length; i++) {
+			recette += tab[i].getNbInscrits() * tab[i].getTarif();			
+		}
+		return recette;		
+	}
+	
+	public int getNbTotalClients() {
+		int clients = 0;
+		for (int i = 0; i < tab.length; i++) {
+			clients += tab[i].getNbInscrits();			
+		}
+		return clients;	
+	}
+	
+	public Spectacle getSpectacle(String nomSpectacle) {
+		for (int i = 0; i < tab.length; i++) {
+			if (tab[i].getNom()==nomSpectacle) {
+				return tab[i];
+			};			
+		}
+		return null;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Spectacle[] getSpectacles() {
+		return tab;
+	}
+
+	public void setSpectacles(Spectacle[] spectacles) {
+		this.tab = spectacles;
+	}
+	
+	
+	
+	
+}
---
+++ src/fr/diginamic/essais/TestTheatrePro.java
@@ -0,0 +1,39 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.entites.Spectacle;
+import fr.diginamic.entites.TheatrePro;
+
+public class TestTheatrePro {
+
+	public static void main(String[] args) {
+		
+		Spectacle[] spectacles = {
+				new Spectacle("C'est arrivé à tous", "Comédie", 6.5, 20, 0),
+				new Spectacle("On est pas bien là ?", "Comédie", 6.5, 20, 0),
+				new Spectacle("On en veut encore", "Comédie", 6.5, 20, 0),
+				new Spectacle("Une rencontre aléatoire", "Drame", 333.5, 2, 1),
+				new Spectacle("Hip-Hop", "Dance", 7.5, 30, 0),
+				new Spectacle("Metallica", "Concert", 45.0, 300, 0),
+		};
+		
+		TheatrePro monTheatre = new TheatrePro("Bienvenue Chez Nous",spectacles);
+		
+		monTheatre.inscrire("C'est arrivé à tous", 10);
+		monTheatre.inscrire("On est pas bien là ?", 3);
+		monTheatre.inscrire("On en veut encore", 3);
+		monTheatre.inscrire("Une rencontre aléatoire", 1);
+		monTheatre.inscrire("Hip-Hop", 6);
+		monTheatre.inscrire("Metallica", 60);
+		
+		System.out.println(monTheatre.getRecetteTotale());
+		System.out.println(monTheatre.getNbTotalClients());
+		
+		
+		//System.out.println(monTheatre.getSpectacles().length);
+		Spectacle newSpe = new Spectacle("Motörhead", "Concert", 45.0, 300, 60);
+		monTheatre.ajouterSpectacle(newSpe);
+		//System.out.println(monTheatre.getSpectacles().length);
+		System.out.println(monTheatre.getSpectacle("Motörhead").getNom());
+	}
+
+}
